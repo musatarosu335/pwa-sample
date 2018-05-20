3d905f2393ce91743c0ce77cb05a681cebc8eb49
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 16:40:37 +0900

    service workerの登録とインストール

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,13 @@
-// とりあえず何もしない
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'test-pwa-cache-v1'; // 任意の値
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+];
+
+// Service Workerのインストール
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => (
+    cache.addAll(urlsToCache)
+  )));
+});
