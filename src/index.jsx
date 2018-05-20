a319018b77bf12720e4041cb476724b29b8b4f73
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 15:56:45 +0900

    Firebaseの設定

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,22 @@
+import firebase from 'firebase/app';
 import React from 'react';
 import ReactDom from 'react-dom';
+import ENV from '../env.json';
 import App from './components/App';
+
+// firebase設定
+const config = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PRJ_ID,
+  storageBucket: ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(config);
+
+require('firebase/storage');
+require('firebase/firestore');
 
 ReactDom.render(
   <App />,
