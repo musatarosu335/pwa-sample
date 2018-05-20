2c2e5df74ac3d12fbaf21468bcc6b74736e1e17a
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 16:55:13 +0900

    キャッシュされたレスポンスを返す

--- public/sw.js
+++ public/sw.js
@@ -11,3 +11,14 @@ self.addEventListener('install', (event) => {
     cache.addAll(urlsToCache)
   )));
 });
+
+// キャッシュされたレスポンスを返す
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((res) => {
+    // 有効なキャッシュがあれば、キャッシュしたリソースを返す
+    if (res) {
+      return res;
+    }
+    return fetch(event.request);
+  }));
+});
