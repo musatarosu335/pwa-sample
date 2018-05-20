53003193415259e7aee6a7e60490406b8c13ac43
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 15:32:21 +0900

    cssをbundleしないように修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 // const MODE = 'development';
 // const enableSourceMap = (MODE === 'development');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const publicDir = path.join(__dirname, '/public');
 module.exports = [
@@ -43,28 +42,5 @@ module.exports = [
       contentBase: publicDir,
     },
     // devtool: '#inline-source-map',
-  },
-  {
-    entry: {
-      style: './stylesheets/index.scss',
-    },
-    output: {
-      path: publicDir,
-      publicPath: '/',
-      filename: 'bundle.css',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            use: [{ loader: 'css-loader', options: { url: false } }, 'sass-loader'],
-          }),
-        },
-      ],
-    },
-    plugins: [
-      new ExtractTextPlugin('bundle.css'),
-    ],
   },
 ];
