cd623251925335198df38f67a1c6babc163029fc
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-20 17:14:01 +0900

    リクエストごとに逐次キャッシュするように修正

--- public/sw.js
+++ public/sw.js
@@ -19,6 +19,21 @@ self.addEventListener('fetch', (event) => {
     if (res) {
       return res;
     }
-    return fetch(event.request);
+    // リクエストごとに逐次キャッシュ
+    // event.requestはStreamなのでcloneする必要がある
+    const fetchRequest = event.request.clone();
+
+    return fetch(fetchRequest).then((res) => {
+      // レスポンスが正しいかをチェック
+      if (!res || res.status !== 200 || res.type !== 'basic') {
+        return res;
+      }
+      // resはStreamなのでcloneする必要がある
+      const responseToCache = res.clone();
+      caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, responseToCache);
+      });
+      return res;
+    });
   }));
 });
