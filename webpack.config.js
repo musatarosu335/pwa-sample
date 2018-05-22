f1b380797d3611f4d348c04a2ea909cc27b927e9
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-22 22:41:16 +0900

    webpack4のmodeによるinline-source-mapの切り替え

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,41 @@
 const path = require('path');
-// const MODE = 'development';
-// const enableSourceMap = (MODE === 'development');
 
 const publicDir = path.join(__dirname, '/public');
-module.exports = [
-  {
-    // mode: 'development',
-    entry: [
-      'babel-polyfill',
-      './src/index.jsx',
-    ],
-    output: {
-      path: publicDir,
-      publicPath: '/',
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: [/\.js$/, /\.jsx$/],
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  ['env', { modules: false }],
-                  'react',
-                ],
-              },
-            },
-          ],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-      historyApiFallback: true,
-      contentBase: publicDir,
-    },
-    // devtool: '#inline-source-map',
+module.exports = (env, argv) => ({
+  entry: [
+    'babel-polyfill',
+    './src/index.jsx',
+  ],
+  output: {
+    path: publicDir,
+    publicPath: '/',
+    filename: 'bundle.js',
   },
-];
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['env', { modules: false }],
+                'react',
+              ],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: publicDir,
+  },
+  devtool: argv.mode === 'development' ? 'inline-source-map' : '',
+});
